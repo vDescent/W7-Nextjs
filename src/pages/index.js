f0b160aabd1ccd2ba2ee2800031afa842712f432
vdescent <timotiusryjaska@gmail.com>
2026-06-30 13:22:24 +0700

    feat: add fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,32 @@
+'use client'
+
+import { useEffect, useState } from "react";
+
 export default function Home(){
+  const [products, setProducts] = useState([]);
+
+  useEffect(()=>{
+    async function fetchData() {
+      try{
+        const response = await fetch('https://fakestoreapi.com/products')
+        const data = await response.json();
+        
+        setProducts(data);
+      } catch(error){
+        console.log(`Error Fetch:`, error);
+      }
+    }
+    fetchData();
+  },[])
+
+  // console.log(products);
+
   return(
     <div className="flex flex-col justify-center items-center">
       <p>testing</p>
-      <p>test commit</p>
+      {products.map((item)=>(
+        <p key={item.id}>{item.title}</p>
+      ))}
     </div>
-  )
+  );
 }
